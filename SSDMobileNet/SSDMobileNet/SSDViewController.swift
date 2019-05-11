f8b14243a82fa5fde3a49ec0c72e71ca74a7772b
Gokhan Namal <gokhanamal@gmail.com>
2019-05-11 17:24:22 +0300

    first commit

---
+++ SSDMobileNet/SSDMobileNet/SSDViewController.swift
@@ -0,0 +1,354 @@
+//
+//  ViewController.swift
+//  yolo-object-tracking
+//
+//  Created by Mikael Von Holst on 2017-12-19.
+//  Copyright © 2017 Mikael Von Holst. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+import AVFoundation
+import Accelerate
+import Speech
+
+class SSDViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate,SFSpeechRecognizerDelegate {
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var frameLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    let semaphore = DispatchSemaphore(value: 1)
+    var lastExecution = Date()
+    var screenHeight: Double?
+    var screenWidth: Double?
+    let ssdPostProcessor = SSDPostProcessor(numAnchors: 1917, numClasses: 90)
+    var visionModel:VNCoreMLModel?
+    var lastString = ""
+    
+    // speech recognizer
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier: "tr-TR"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    var isRecording = false
+
+    
+    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    private lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.hd1280x720
+        
+        guard
+            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: backCamera)
+            else { return session }
+        session.addInput(input)
+        return session
+    }()
+
+    let numBoxes = 100
+    var boundingBoxes: [BoundingBox] = []
+    let multiClass = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.cameraView?.layer.addSublayer(self.cameraLayer)
+        self.cameraView?.bringSubview(toFront: self.frameLabel)
+        self.frameLabel.textAlignment = .left
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
+        self.captureSession.addOutput(videoOutput)
+        self.captureSession.startRunning()
+        setupVision()
+        
+        setupBoxes()
+        
+        screenWidth = Double(view.frame.width)
+        screenHeight = Double(view.frame.height)
+        
+        self.requestSpeechAuthorization()
+    }
+    
+    @IBAction func start(_ sender: Any) {
+        if isRecording == true {
+            audioEngine.stop()
+            recognitionTask?.cancel()
+            isRecording = false
+        } else {
+            self.recordAndRecognizeSpeech()
+            isRecording = true
+        }
+    }
+    
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        if node != nil {
+            let recordingFormat = node.outputFormat(forBus: 0)
+            node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+                self.request.append(buffer)
+            }
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            self.sendAlert(message: "There has been an audio engine error.")
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            self.sendAlert(message: "Speech recognition is not supported for your current locale.")
+            return
+        }
+        if !myRecognizer.isAvailable {
+            self.sendAlert(message: "Speech recognition is not currently available. Check back at a later time.")
+            // Recognizer is not available right now
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                
+                let bestString = result.bestTranscription.formattedString
+                self.frameLabel.text = bestString
+                
+                var lastString: String = ""
+                for segment in result.bestTranscription.segments {
+                    let indexTo = bestString.index(bestString.startIndex, offsetBy: segment.substringRange.location)
+                    lastString = bestString.substring(from: indexTo)
+                }
+            } else if let error = error {
+                self.sendAlert(message: "There has been a speech recognition error.")
+                print(error)
+            }
+        })
+    }
+    
+    //MARK: - Check Authorization Status
+    
+    func requestSpeechAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.startButton.isEnabled = true
+                case .denied:
+                    self.startButton.isEnabled = false
+                    let error = "User denied access to speech recognition"
+                    self.speak(name: error)
+                case .restricted:
+                    self.startButton.isEnabled = false
+                    let error = "Speech recognition restricted on this device"
+                    self.speak(name: error)
+                case .notDetermined:
+                    self.startButton.isEnabled = false
+                    let error = "Speech recognition not yet authorized"
+                    self.speak(name: error)
+                }
+            }
+        }
+    }
+    
+    func sendAlert(message: String) {
+        let alert = UIAlertController(title: "Speech Recognizer Error", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        cameraLayer.frame = cameraView.layer.bounds
+    }
+    
+    func setupBoxes() {
+        // Create shape layers for the bounding boxes.
+        for _ in 0..<numBoxes {
+            let box = BoundingBox()
+            box.addToLayer(view.layer)
+            self.boundingBoxes.append(box)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.cameraLayer.frame = self.cameraView?.bounds ?? .zero
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: ssd_mobilenet_feature_extractor().model)
+            else { fatalError("Can't load VisionML model") }
+        self.visionModel = visionModel
+    }
+    
+    func processClassifications(for request: VNRequest, error: Error?) -> [Prediction]? {
+        let thisExecution = Date()
+        let executionTime = thisExecution.timeIntervalSince(lastExecution)
+        let framesPerSecond:Double = 1/executionTime
+        lastExecution = thisExecution
+        guard let results = request.results as? [VNCoreMLFeatureValueObservation] else {
+            return nil
+        }
+        guard results.count == 2 else {
+            return nil
+        }
+        guard let boxPredictions = results[1].featureValue.multiArrayValue,
+            let classPredictions = results[0].featureValue.multiArrayValue else {
+            return nil
+        }
+        
+        let predictions = self.ssdPostProcessor.postprocess(boxPredictions: boxPredictions, classPredictions: classPredictions)
+        return predictions
+    }
+
+    func drawBoxes(predictions: [Prediction]) {
+        
+        for (index, prediction) in predictions.enumerated() {
+            if let classNames = self.ssdPostProcessor.classNames {
+                print("Class: \(classNames[prediction.detectedClass])")
+                
+                let textColor: UIColor
+                let name: String = classNames[prediction.detectedClass]
+                let textLabel = String(format: "%.2f - %@", self.sigmoid(prediction.score), name)
+                
+                self.speak(name: name)
+                
+                textColor = UIColor.black
+                let rect = prediction.finalPrediction.toCGRect(imgWidth: self.screenWidth!, imgHeight: self.screenWidth!, xOffset: 0, yOffset: (self.screenHeight! - self.screenWidth!)/2)
+                self.boundingBoxes[index].show(frame: rect,
+                                               label: textLabel,
+                                               color: UIColor.red, textColor: textColor)
+            }
+        }
+        for index in predictions.count..<self.numBoxes {
+            self.boundingBoxes[index].hide()
+        }
+    }
+    
+    func speak(name: String){
+        let utterance = AVSpeechUtterance(string: name)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        let synth = AVSpeechSynthesizer()
+        synth.speak(utterance)
+
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        guard let visionModel = self.visionModel else {
+            return
+        }
+
+        var requestOptions:[VNImageOption : Any] = [:]
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics:cameraIntrinsicData]
+        }
+        let orientation = CGImagePropertyOrientation(rawValue: UInt32(EXIFOrientation.rightTop.rawValue))
+
+        let trackingRequest = VNCoreMLRequest(model: visionModel) { (request, error) in
+            guard let predictions = self.processClassifications(for: request, error: error) else { return }
+            DispatchQueue.main.async {
+                self.drawBoxes(predictions: predictions)
+            }
+            self.semaphore.signal()
+        }
+        trackingRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+
+        
+        self.semaphore.wait()
+        do {
+            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: orientation!, options: requestOptions)
+            try imageRequestHandler.perform([trackingRequest])
+        } catch {
+            print(error)
+            self.semaphore.signal()
+            
+        }
+    }
+
+    func sigmoid(_ val:Double) -> Double {
+         return 1.0/(1.0 + exp(-val))
+    }
+
+    func softmax(_ values:[Double]) -> [Double] {
+        if values.count == 1 { return [1.0]}
+        guard let maxValue = values.max() else {
+            fatalError("Softmax error")
+        }
+        let expValues = values.map { exp($0 - maxValue)}
+        let expSum = expValues.reduce(0, +)
+        return expValues.map({$0/expSum})
+    }
+    
+    public static func softmax2(_ x: [Double]) -> [Double] {
+        var x:[Float] = x.compactMap{Float($0)}
+        let len = vDSP_Length(x.count)
+        
+        // Find the maximum value in the input array.
+        var max: Float = 0
+        vDSP_maxv(x, 1, &max, len)
+        
+        // Subtract the maximum from all the elements in the array.
+        // Now the highest value in the array is 0.
+        max = -max
+        vDSP_vsadd(x, 1, &max, &x, 1, len)
+        
+        // Exponentiate all the elements in the array.
+        var count = Int32(x.count)
+        vvexpf(&x, x, &count)
+        
+        // Compute the sum of all exponentiated values.
+        var sum: Float = 0
+        vDSP_sve(x, 1, &sum, len)
+        
+        // Divide each element by the sum. This normalizes the array contents
+        // so that they all add up to 1.
+        vDSP_vsdiv(x, 1, &sum, &x, 1, len)
+        
+        let y:[Double] = x.compactMap{Double($0)}
+        return y
+    }
+    
+    enum EXIFOrientation : Int32 {
+        case topLeft = 1
+        case topRight
+        case bottomRight
+        case bottomLeft
+        case leftTop
+        case rightTop
+        case rightBottom
+        case leftBottom
+        
+        var isReflect:Bool {
+            switch self {
+            case .topLeft,.bottomRight,.rightTop,.leftBottom: return false
+            default: return true
+            }
+        }
+    }
+    
+    func compensatingEXIFOrientation(deviceOrientation:UIDeviceOrientation) -> EXIFOrientation
+    {
+        switch (deviceOrientation) {
+        case (.landscapeRight): return .bottomRight
+        case (.landscapeLeft): return .topLeft
+        case (.portrait): return .rightTop
+        case (.portraitUpsideDown): return .leftBottom
+            
+        case (.faceUp): return .rightTop
+        case (.faceDown): return .rightTop
+        case (_): fallthrough
+        default:
+            NSLog("Called in unrecognized orientation")
+            return .rightTop
+        }
+    }
+}
+
